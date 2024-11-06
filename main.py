a55ccb9aa8a53a69b3792e59b788628e9d50029e
Samuel <61188554+samibanks01@users.noreply.github.com>
2024-11-06 02:15:57 -0900

    Create main.py
    Added solution

---
+++ main.py
@@ -0,0 +1,19 @@
+print("= Marvel Movie Character Creator =")
+
+wanderer = input("Do you like 'hanging around'?:")
+if wanderer == "yes":
+  print("Then you're Spider-man")
+else:
+  print("Excellent choice.")
+
+boldman = input("Do you have a 'gravelly' voice?:")
+if boldman == "yes":
+  print("Aww, then you're not Korg")
+else:
+  print("Excellent choice.")
+
+crawler = input("Do you often feel 'Marvelous'?:")
+if crawler == "yes":
+  print("Aha! You're Captain Marvel! Hi!")
+else:
+  print("Sorry, Can't Identify you!.")
